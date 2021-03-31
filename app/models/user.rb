0ccf8a385e2de78997bc418df27b2982762905b3
Andrehochi <andrehochiwork@gmail.com>
2021-03-31 13:28:43 -0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_many :locations, through: :orders
     has_secure_password
     validates :username, uniqueness: true
-    
+    validates :balance, numericality: {greater_than_or_equal_to: 0}
+    validates :balance, reward_points: {greater_than_or_equal_to: 0}
 end
