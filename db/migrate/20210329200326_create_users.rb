424b97e0bda604b145906265f3c0aad3aea3801e
Bryecook <brybcook@gmail.com>
2021-04-01 09:10:55 -0700

    Photos

--- db/migrate/20210329200326_create_users.rb
+++ db/migrate/20210329200326_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :password_digest
       t.integer :reward_points
       t.float :balance
+      t.string :profile_picture, :default => "https://i.pinimg.com/originals/3b/17/fe/3b17fe518d484d23e5da184f25327fa1.jpg"
 
       t.timestamps
     end
